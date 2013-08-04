e0f4f6799eafa4894288190ce37b949c1bda2d4a
Daphne Chong <daphne.h.chong@gmail.com>
2013-08-04 14:10:21 +1000

    Initial Commit

---
+++ FinalProject/Mi9AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  Mi9AppDelegate.h
+//  FinalProject
+//
+//  Created by Daphne Chong on 4/08/13.
+//  Copyright (c) 2013 Daphne Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Mi9ViewController;
+
+@interface Mi9AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) Mi9ViewController *viewController;
+
+@end
---
+++ FinalProject/Mi9ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Mi9ViewController.h
+//  FinalProject
+//
+//  Created by Daphne Chong on 4/08/13.
+//  Copyright (c) 2013 Daphne Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Mi9ViewController : UIViewController
+
+@end
